06cafbb95a962f5b47fffbec90597e1a4b80c672
mudzereli <mudz@charter.net>
2024-12-07 08:55:19 -0600

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -123,6 +123,8 @@ def scroll_and_capture_decks_incrementally(driver, decks, scroll_pause_time=0, s
 def capture_deck_tiles(driver, decks):
     deck_tiles = driver.find_elements(By.CLASS_NAME, "deck-tile")
     for tile in deck_tiles:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "deck-tile")))
+
         deck = Deck()
         deck.timestamp = timestamp
         deck.format = gameformat
